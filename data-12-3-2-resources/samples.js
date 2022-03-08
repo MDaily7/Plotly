bddd970018623dcdafe783ec0f54865cbabd08a2
MDaily7 <mdaily622@gmail.com>
2022-03-08 11:25:12 -0800

    data folder added with an index.html, samples.js, and samples.json

---
+++ data-12-3-2-resources/samples.js
@@ -0,0 +1,10 @@
+/*  d3.json("samples.json").then(function(data) {
+    wfreq = data.metadata.map(person=> person.wfreq).sort((a,b)=> b - a); 
+    filteredWFreq = wfreq.filter(element => element != null);
+     console.log(filteredWFreq);
+});  */
+
+//d3.json('samples.json').then(data=> console.log(data));
+
+d3.json('samples.json').then(data => Object.entries(data.metadata[0]).forEach(([key, value])=> console.log(key + ': ' + value)));
+ 
