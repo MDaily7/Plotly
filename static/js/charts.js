c395e9368693e8f86fdf7189450da22aa3df6af0
MDaily7 <mdaily622@gmail.com>
2022-03-20 20:22:03 -0700

    all charts added to charts.js, styling added in style.css

--- static/js/charts.js
+++ static/js/charts.js
@@ -72,22 +72,77 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,10).map(id=> 'OTU' + id).reverse()
+    var yticks = otu_ids.slice(0,10).map(id=> 'OTU ' + id).reverse()
 
     // 8. Create the trace for the bar chart. 
     var barData = [{type: 'bar',
     x:sample_values.slice(0,10).reverse(),
     y:yticks,
-    orientation: 'h'
+    orientation: 'h',
+    text: otu_labels.slice(0,10).reverse()
 
     }
       
     ];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {title: 'Top Ten Bacteria Cultures Found'
+    var barLayout = {title: 'Top Ten Bacteria Cultures Found',
+    paper_bgcolor:'lavender'
      
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot('bar', barData, barLayout)
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+  mode: 'markers',
+marker: {
+  color: otu_ids,
+  size: sample_values
+}}
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = { title: 'Bacteria Cultures Per Sample',
+    xaxis: {title: 'OTU ID'},
+    yaxis: {title: 'Sample Value'},
+    hovermode: 'closest',
+    paper_bgcolor:'lavender'
+
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout); 
+
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(object=> object.id == sample);
+    var result = resultArray[0];
+    var wfreq = parseFloat(result.wfreq);
+    var guageData = [{
+      domain: {x:[0,1], y:[0,1]},
+      value: wfreq,
+      title: {text: 'Belly Button Washing Frequency<br> Scrubs per Week'},
+      type: 'indicator',
+      mode: 'gauge+number',
+      gauge: {axis: {range: [0, 10]},
+       bar: {color:'black'},
+       steps: [
+         {range: [0, 2], color:'red'},
+         {range: [2, 4], color:'orange'},
+         {range: [4, 6], color:'yellow'},
+         {range: [6, 8], color:'green'},
+         {range: [8, 10], color:'purple'}
+       ]
+      }
+
+       }];
+    var gaugeLayout = {width: 600, height: 500, margin: {t:0, b:0}, paper_bgcolor:'lavender'};
+
+    Plotly.newPlot('gauge', guageData, gaugeLayout);
+
+
   });
 }
